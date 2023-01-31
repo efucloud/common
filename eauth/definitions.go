27295409053804e5ea746a19cec0ee114c4519d4
kubeall <kubeall@aliyun.com>
2023-01-31 19:47:36 +0800

    update

--- eauth/definitions.go
+++ eauth/definitions.go
@@ -60,15 +60,17 @@ type ApplicationSyncAccountInfo struct {
 	Groups           []string               `json:"groups" yaml:"groups"`
 }
 type AccountClaims struct {
-	AccountID    uint   `json:"accountId" `
-	Org          string `json:"org"`
-	AuthProvider string `json:"authProvider" `
-	Username     string `json:"username"` // 用户名 组织内唯一必须由DNS-1123标签格式的单元组成
-	Nickname     string `json:"nickname"` // 昵称，如中文名
-	OrgAdmin     uint   `json:"orgAdmin"` // 是否为组织管理员，内建组织具有所有组织的权限
-	Nonce        string `json:"nonce"`
-	Email        string `json:"email"`
-	Phone        string `json:"phone"`
+	AccountID       uint     `json:"accountId" `
+	Org             string   `json:"org"`
+	AuthProvider    string   `json:"authProvider"`
+	Username        string   `json:"username"` // 用户名 组织内唯一必须由DNS-1123标签格式的单元组成
+	Nickname        string   `json:"nickname"` // 昵称，如中文名
+	OrgAdmin        uint     `json:"orgAdmin"` // 是否为组织管理员，内建组织具有所有组织的权限
+	Nonce           string   `json:"nonce"`
+	Email           string   `json:"email"`
+	Phone           string   `json:"phone"`
+	Groups          []string `json:"groups" yaml:"groups"`
+	AuthApplication string   `json:"authApplication" yaml:"authApplication"`
 	jwt.RegisteredClaims
 }
 
