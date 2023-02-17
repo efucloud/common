b401fe18080ad95001bfd919f1a6357fcf161f27
kubeall <kubeall@aliyun.com>
2023-02-17 10:33:12 +0800

    update

--- eauth/definitions.go
+++ eauth/definitions.go
@@ -41,7 +41,7 @@ type UserInfo struct {
 	AuthProvider     string                 `json:"authProvider"`     // 认证提供商
 	Username         string                 `json:"username"`         // 用户名 组织内唯一必须由DNS-1123标签格式的单元组成
 	Nickname         string                 `json:"nickname"`         // 昵称，如中文名
-	OrgAdmin         uint                   `json:"orgAdmin"`         // 是否为组织管理员，内建组织具有所有组织的权限
+	OrgRole          string                 `json:"orgRole"`          //组织角色
 	Phone            string                 `json:"phone"`
 	ID               uint                   `json:"id"`
 }
