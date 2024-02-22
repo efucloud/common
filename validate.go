13a1192787c5274e5b98246a13a1cd8c84e93ac5
cloudy <kubeall@aliyun.com>
2024-02-22 16:22:12 +0800

    add validate func

--- validate.go
+++ validate.go
@@ -21,7 +21,9 @@ import (
 	"github.com/go-playground/validator/v10"
 	"reflect"
 	"regexp"
+	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -111,4 +113,51 @@ func CodeValidate(fl validator.FieldLevel) bool {
 		return false
 	}
 	return RFC1123Reg.MatchString(code)
+}
+
+func NotOneOf(fl validator.FieldLevel) bool {
+	vals := parseOneOfParam2(fl.Param())
+
+	field := fl.Field()
+
+	var v string
+	switch field.Kind() {
+	case reflect.String:
+		v = field.String()
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		v = strconv.FormatInt(field.Int(), 10)
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		v = strconv.FormatUint(field.Uint(), 10)
+	default:
+		panic(fmt.Sprintf("Bad field type %T", field.Interface()))
+	}
+	for i := 0; i < len(vals); i++ {
+		if vals[i] == v {
+			return false
+		}
+	}
+	return true
+}
+
+var (
+	oneofValsCache         = map[string][]string{}
+	oneofValsCacheRWLock   = sync.RWMutex{}
+	splitParamsRegexString = `'[^']*'|\S+`
+	splitParamsRegex       = regexp.MustCompile(splitParamsRegexString)
+)
+
+func parseOneOfParam2(s string) []string {
+	oneofValsCacheRWLock.RLock()
+	vals, ok := oneofValsCache[s]
+	oneofValsCacheRWLock.RUnlock()
+	if !ok {
+		oneofValsCacheRWLock.Lock()
+		vals = splitParamsRegex.FindAllString(s, -1)
+		for i := 0; i < len(vals); i++ {
+			vals[i] = strings.Replace(vals[i], "'", "", -1)
+		}
+		oneofValsCache[s] = vals
+		oneofValsCacheRWLock.Unlock()
+	}
+	return vals
 }
